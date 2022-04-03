bc476f4b421734696c3028141cb2b2a8708a8b9f
Clifford <cliffords2105@gmail.com>
2022-04-03 18:14:36 +0200

    API created

--- project/api/urls.py
+++ project/api/urls.py
@@ -3,11 +3,21 @@ from . import views
 
 urlpatterns = [
     path("projects/", views.show_projects, name = "show_projects"),
+    path("projects/create/", views.create_project, name = "create_project"),
     path("projects/details/<int:id>/", views.project_details, name = "project_details"),
     path("projects/edit/<int:id>/", views.edit_project, name = "edit_project"),
     path("projects/delete/<int:id>/", views.delete_project, name = "delete_project"),
+    path("projects/web2/", views.show_web2_projects, name = "show_web2"),
+    path("projects/web3/", views.show_web3_projects, name = "show_web3"),
+    path("projects/data-science/", views.show_data_science, name = "show_data_science"),
+
+
     path("articles/", views.show_articles, name = "show_articles"),
+    path("articles/create/", views.create_article, name = "cretae_article"),
     path("articles/details/<int:id>/", views.article_details, name = "article_details"),
     path("articles/edit/<int:id>/", views.edit_article, name = "edit_article"),
-    path("articles/delete/<int:id>/", views.delete_article, name = "delete_article")
+    path("articles/delete/<int:id>/", views.delete_article, name = "delete_article"),
+    path("articles/software-development/", views.show_SD_articles, name = "show_SD_articles"),
+    path("articles/business/", views.show_business_articles, name = "show_business"),
+    path("articles/investments/", views.show_investments_articles, name = "show_investments"),
 ]
--- project/api/views.py
+++ project/api/views.py
@@ -8,18 +8,20 @@ from .serializers import ProjectSerializer, ArticleSerializer
 
 
 #------------- PROJECTS ---------------------#
-@api_view(["GET", "POST"])
-def show_projects(request):
-    if request.method == "POST":
-        serialized_data = ProjectSerializer(data = request.data)
+@api_view(["POST"])
+def create_project(request):
+    serialized_data = ProjectSerializer(data = request.data)
 
-        if serialized_data.is_valid():
-            serialized_data.save()
-        else:
-            return HttpResponse("Please check your data")
-    else:
-        data = Project.objects.all()
-        serialized_data = ProjectSerializer( data = data, many = True)
+    if serialized_data.is_valid():
+        serialized_data.save()
+    return Response(serialized_data.data)
+
+    
+
+@api_view(["GET"])
+def show_projects(request):
+    data = Project.objects.all()
+    serialized_data = ProjectSerializer(data, many = True)
     return Response(serialized_data.data)
    
 
@@ -32,27 +34,60 @@ def project_details(request, id):
 
 @api_view(["PUT"])
 def edit_project(request, id):
-    return HttpResponse("Edit project " + str(id))
+    project = Project.objects.get(id = id)
+    data = Project.objects.get(id = id)
+    serialized_data = ProjectSerializer(instance = project, data = request.data)
+
+    if serialized_data.is_valid():
+        serialized_data.save()
+    return Response(serialized_data.data)
+
 
 @api_view(["DELETE"])
 def delete_project(request, id):
     data = Project.objects.get(pk = id)
     data.delete()
+    return HttpResponse("Project deleted")
+
+
+@api_view(["GET"])
+def show_web2_projects(request):
+    projects = Project.objects.filter(category = "web 2.0")
+    serialized_data = ProjectSerializer(projects, many = True)
+
+    return Response(serialized_data.data)
+
+@api_view(["GET"])
+def show_web3_projects(request):
+    projects = Project.objects.filter(category = "web 3.0")
+    serialized_data = ProjectSerializer(projects, many = True)
+
+    return Response(serialized_data.data)
+
+@api_view(["GET"])
+def show_data_science(request):
+    projects = Project.objects.filter(category = "data science")
+    serialized_data = ProjectSerializer(projects, many = True)
+
+    return Response(serialized_data.data)
 
 
 
 #------------ ARTICLES --------------------------#
-@api_view(["GET", "POST"])
+@api_view(["GET"])
 def show_articles(request):
-    if request.method == "POST":
-        serilized_data = ArticleSerializer(data = request.data, many = False)
-        if serialized_data.is_valid():
-            serialized_data.save()
-    else:
-        data = Article.objects.all()
-        serialized_data = ArticleSerializer(data, many = True)
+    data = Article.objects.all()
+    serialized_data = ArticleSerializer(data, many = True)
     return Response(serialized_data.data)
-        
+
+@api_view(["POST"])
+def create_article(request):
+    data = request.data 
+    serialized_data = ArticleSerializer(data = data, many = False)
+
+    if serialized_data.is_valid():
+        serialized_data.save()   
+    return HttpResponse("Please check your data.") 
         
 
 @api_view(["GET"])
@@ -61,11 +96,39 @@ def article_details(request, id):
     serialized_data = ArticleSerializer(data)
     return Response(serialized_data.data)
 
-
+@api_view(["PUT"])
 def edit_article(request, id):
-    return HttpResponse("Edit article", str(id))
+    article = Article.objects.get(id = id)
+    data = request.data 
+    serialized_data = ArticleSerializer(instance = article, data = data)
+
+    if serialized_data.is_valid():
+        serialized_data.save()
+    return HttpResponse("Article updated")
+    
 
 @api_view(["DELETE"])
 def delete_article(request, id):
     data = Article.objects.get(pk = id)
-    data.save()
+    data.delete()
+
+@api_view(["GET"])
+def show_SD_articles(request):
+    articles = Article.objects.filter(category = "software development")
+    serialized_data = ArticleSerializer(articles, many =True)
+
+    return Response(serialized_data.data)
+
+@api_view(["GET"])
+def show_business_articles(request):
+    articles = Article.objects.filter(category = "business")
+    serialized_data = ArticleSerializer(articles, many =True)
+
+    return Response(serialized_data.data)
+
+@api_view(["GET"])
+def show_investments_articles(request):
+    articles = Article.objects.filter(category = "investments")
+    serialized_data = ArticleSerializer(articles, many =True)
+
+    return Response(serialized_data.data)
